327a56db8421c00cc9360f02160c32cfe5b5250b
nmitra6 <nmitra6@hotmail.com>
2021-05-28 22:47:16 +0530

    link activated

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 4000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>Coming soon ...</h1><div style="padding-left: 50px"><p><!--<a href="http://dev.nitdian.org">View progress</a>--!></p></div></body></html>');
+    res.end('<html><body><h1>Coming soon ...</h1><div style="padding-left: 50px"><p><a href="https://dev.nitdian.org">View progress</a></p></div></body></html>');
 });
 
 server.listen(port, hostname, () => {
